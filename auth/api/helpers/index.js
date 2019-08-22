ff0806cf5aa223da76b60bc9e8b1df0512e4741b
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-08-22 08:36:45 +0700

    Huft, fixing HTTP error response: "Cannot set headers after they are sent to the client"
    Change sender response to the final of the promise.

--- auth/api/helpers/index.js
+++ auth/api/helpers/index.js
@@ -4,7 +4,7 @@ const ip         = require('ip');
 const UsersModel = require('../models/users');
 const AuthModel  = require('../models/auth');
 
-exports.addUserToDB = data => {
+exports._addUserToDB = data => {
 	const user = new UsersModel({
 		first_name: data.first_name,
 		last_name: data.last_name,
@@ -15,25 +15,25 @@ exports.addUserToDB = data => {
 	})
 
 	const result = user.save()
-			.then(res => {
-				return res;
-			})
-			.catch(err => {
-					console.log(err);
-					return err;
-			})
+		.then(res => {
+			return res;
+		})
+		.catch(err => {
+				return err;
+		})
 
 	return result;
 }
 
-exports.checkEmailinDB = email => {
+exports._checkEmailinDB = email => {
 	const result = UsersModel.findOne(
 		{ 'auth.email': email }
 	)
+
 	return result;
 }
 
-exports.authSession = (res, data, ua) => {
+exports._authSession = (res, data, ua) => {
 	const auth = new AuthModel({
 		user_id: res.id,
 		device_info: platform.parse(ua),
@@ -47,8 +47,8 @@ exports.authSession = (res, data, ua) => {
 			return response;
 		})
 		.catch(err => {
-			console.log(err);
 			return err;
 		})
+
 	return result;
 }
--- auth/api/index.js
+++ auth/api/index.js
@@ -3,25 +3,41 @@ const Promise = require('bluebird');
 const platform   = require('platform');
 var router = express.Router(); 
 
-const { addUserToDB, authSession, checkEmailinDB } = require('./helpers');
+const { _addUserToDB, _authSession, _checkEmailinDB } = require('./helpers');
+const { registerResponse, errorResponse } = require('./utils/responsers');
 
 router.post('/auth/register', function(req, res, next) {
   Promise.try(async () => {
-    const result = await checkEmailinDB(req.body.email)
+    const result = await _checkEmailinDB(req.body.email)
     return result;
   })
   .then(response => {
     if(!response) {
-      const userAdded = addUserToDB(req.body);
-      console.log(userAdded);
+      const userAdded = _addUserToDB(req.body);
       return userAdded;
     } else {
-      res.send(response);
+      return 'user is exist';
     }
   })
   .then(async response => {
-    const authAdded = await authSession(response, req.body, req.headers['user-agent']);
-    res.send(authAdded);
+    console.log(response);
+    try {
+      if (response == 'user is exist') {
+        res.status(401).send(errorResponse(
+          `Email ${req.body.email} has been taken, please make sure you have another email.`,
+          401
+        ))
+      } else {
+        const authAdded = await _authSession(response, req.body, req.headers['user-agent']);
+        res.status(201).send(registerResponse(
+          'Success registered user.',
+          201,
+          authAdded
+        ))
+      } 
+    } catch (err) {
+      res.send('error when insert auth to db. ' + err);
+    }
   })
   .catch(err => res.send(err))
 });
