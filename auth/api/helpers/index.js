44472965a0df527148991bc369241a8499d25f03
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-12-04 22:45:40 +0700

    Updated

--- auth/api/helpers/index.js
+++ auth/api/helpers/index.js
@@ -1,54 +1,92 @@
 const mongoose   = require('mongoose');
 const platform   = require('platform');
 const ip         = require('ip');
+const bcrypt     = require('bcrypt');
+const jwt        = require('jsonwebtoken');
+const _          = require('lodash');
 const UsersModel = require('../models/users');
 const AuthModel  = require('../models/auth');
 
-exports._addUserToDB = data => {
-	const user = new UsersModel({
-		first_name: data.first_name,
-		last_name: data.last_name,
-		auth: {
-			email: data.email,
-			password: data.password
-		},
-	})
+const { registerResponse, errorResponse } = require('../utils/responsers');
 
-	const result = user.save()
-		.then(res => {
-			return res;
+exports._REGISTER_USER = (data, ua) => {
+	const VALIDATION = body => {
+		return new Promise((resolve, reject) => {
+			if(_.isEmpty(body)) {
+				console.log('Body is empty')
+				reject();
+			}
+			resolve(body);
 		})
-		.catch(err => {
+	}
+
+	function checkEmailinDB(payload) {
+		const result = UsersModel.findOne({ 'auth.email': payload.email })
+		return result;
+	}
+
+	async function addUsertoDB(payload, body) {
+		if(!payload) {
+			const hashingPassword = bcrypt.hashSync(body.password, 10);
+			const user = new UsersModel({
+				first_name: body.first_name,
+				last_name: body.last_name,
+				auth: {
+					email: body.email,
+					password: hashingPassword
+				},
+			})
+
+			const result = await user.save()
+				.then(res => {
+					return res;
+				})
+				.catch(err => {
+						return err;
+				})
+
+			console.log(result);
+			return result;
+		} else {
+			return errorResponse(
+				`Email ${body.email} has been taken. Please make sure you have another email.`,
+				401
+			)
+		}
+	}
+
+	async function addAuthtoDB(payload, user_agent) {
+		const auth = new AuthModel({
+			user_id: payload.id,
+			device_info: platform.parse(user_agent),
+			access_token: '1234',
+			auth_type: 'email',
+			ip_location: ip.address()
+		})
+	
+		const result = await auth.save()
+			.then(response => {
+				return response;
+			})
+			.catch(err => {
 				return err;
-		})
+			})
+	
+		return registerResponse(
+			'Success added user to DB',
+			201,
+			result
+		);
+	}
 
-	return result;
-}
-
-exports._checkEmailinDB = email => {
-	const result = UsersModel.findOne(
-		{ 'auth.email': email }
-	)
-
-	return result;
-}
-
-exports._authSession = (res, data, ua) => {
-	const auth = new AuthModel({
-		user_id: res.id,
-		device_info: platform.parse(ua),
-		access_token: '1234',
-		auth_type: 'email',
-		ip_location: ip.address()
-	})
-
-	const result = auth.save()
-	  .then(response => {
-			return response;
-		})
+	return VALIDATION(data)
+		.then(res => checkEmailinDB(res))
+		.then(res => addUsertoDB(res, data))
+		.then(res => addAuthtoDB(res, ua))
 		.catch(err => {
-			return err;
+			errorResponse(
+				`Error in VALIDATION: ${err}`,
+				500
+			)
 		})
-
-	return result;
 }
--- auth/api/index.js
+++ auth/api/index.js
@@ -3,58 +3,16 @@ const Promise = require('bluebird');
 const platform   = require('platform');
 var router = express.Router(); 
 
-const { _addUserToDB, _authSession, _checkEmailinDB } = require('./helpers');
-const { registerResponse, errorResponse } = require('./utils/responsers');
+const { _REGISTER_USER } = require('./helpers');
+const { errorResponse } = require('./utils/responsers');
 
-router.post('/auth/register', function(req, res, next) {
-  Promise.try(async () => {
-    const result = await _checkEmailinDB(req.body.email)
-    return result;
-  })
-  .then(response => {
-    if(!response) {
-      const userAdded = _addUserToDB(req.body);
-      return userAdded;
-    } else {
-      return 'user is exist';
-    }
-  })
-  .then(async response => {
-    console.log(response);
-    try {
-      if (response == 'user is exist') {
-        res.status(401).send(errorResponse(
-          `Email ${req.body.email} has been taken, please make sure you have another email.`,
-          401
-        ))
-      } else {
-        const authAdded = await _authSession(response, req.body, req.headers['user-agent']);
-        res.status(201).send(registerResponse(
-          'Success registered user.',
-          201,
-          authAdded
-        ))
-      } 
-    } catch (err) {
-      res.send('error when insert auth to db. ' + err);
-    }
-  })
-  .catch(err => res.send(err))
-});
-
-router.get('/auth/test', function(req, res, next) {
-  var ua = req.headers['user-agent'];
-
-	var info = platform.parse(ua);
-	let platformSpec = {
-		name: info.name,
-		version: info.version,
-		layout: info.layout,
-		os: info.os,
-		description: info.description
-	}
-
-	res.send(platformSpec);
+router.post('/auth/register_2', function(req, res) {
+  Promise.try(() => _REGISTER_USER(req.body, req.headers['user-agent']))
+  .then(response => res.status(response.status).json(response))
+  .catch(err => res.send(errorResponse(
+    `Failed to _REGISTER_USER: ${err}`,
+    500
+  )))
 })
 
 module.exports = router;
